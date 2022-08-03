c75408a244e1754041e9c347575321d1f1d70fc3
hyunjin <gju08116@hanyang.ac.kr>
2022-08-03 15:12:42 +0900

    First commit

---
+++ stt.py
@@ -0,0 +1,19 @@
+import sys
+import requests
+client_id = "pficodqpxs"
+client_secret = "aFVah3wAvD8xvbM8Mp4iOUEl9a5h23XlAaDr4gMk"
+lang = "Kor" # 언어 코드 ( Kor, Jpn, Eng, Chn )
+url = "https://naveropenapi.apigw.ntruss.com/recog/v1/stt?lang=" + lang
+
+data = open('1111.mp3', 'rb')
+headers = {
+    "X-NCP-APIGW-API-KEY-ID": client_id,
+    "X-NCP-APIGW-API-KEY": client_secret,
+    "Content-Type": "application/octet-stream"
+}
+response = requests.post(url,  data=data, headers=headers)
+rescode = response.status_code
+if(rescode == 200):
+    print (response.text)
+else:
+    print("Error : " + response.text)
