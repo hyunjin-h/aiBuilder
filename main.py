271432f33e2d83e419cc8d0e50546c3f9588e8a2
hyunjin <gju08116@hanyang.ac.kr>
2022-09-23 22:47:33 +0900

    sever connect

--- main.py
+++ main.py
@@ -9,6 +9,35 @@ class WindowClass(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.mainList.itemDoubleClicked.connect(self.chkItemDoubleClicked)
+        self.runBtn.clicked.connect(self.runModel)
+
+    def chkItemDoubleClicked(self):
+        rowNum=self.mainList.currentRow()
+        rowText=self.mainList.currentItem().text()
+        print(str(rowNum) + " : " + rowText)
+        if(rowText=='text'):
+            print(rowText)
+        elif(rowText=='image'):
+            print(rowText)
+        elif(rowText=='mic'):
+            print(rowText)
+        elif(rowText=='camera'):
+            print(rowText)
+        elif(rowText=='draw'):
+            print(rowText)
+        elif(rowText=='image'):
+            print(rowText)
+        elif(rowText=='image'):
+            print(rowText)
+        elif(rowText=='image'):
+            print(rowText)
+
+    def runModel(self):
+        rowNum=self.mainList.currentRow()
+
+
+
 
 
 if __name__ == "__main__":
